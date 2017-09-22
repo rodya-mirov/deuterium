0576a44d9df9a072edeb39b05d881d9046380400
Richard Rast <Richard.Rast@trinetx.com>
2017-09-21 21:58:07 -0400

    Works

--- src/problems/p101_150.rs
+++ src/problems/p101_150.rs
@@ -1,7 +1,6 @@
 use std::collections::{HashSet, HashMap, BinaryHeap};
 use std::fs::File;
 use std::io::Read;
-use std::rc::Rc;
 
 use num::bigint::{BigUint, BigInt};
 use num::integer::{lcm};
@@ -59,13 +58,30 @@ pub fn p104() -> String {
 
 
 pub fn p107() -> String {
-    let (grid, original_total_weight) = {
+    use std::cmp::{Ord, PartialOrd, Ordering};
+
+    #[derive(Eq, PartialEq)]
+    struct Edge { start: usize, end: usize, weight: u64 }
+
+    impl Ord for Edge {
+        // reversed order, to turn the max heap into a min heap
+        fn cmp(&self, other: &Edge) -> Ordering {
+            other.weight.cmp(&self.weight)
+        }
+    }
+
+    impl PartialOrd for Edge {
+        fn partial_cmp(&self, other: &Edge) -> Option<Ordering> {
+            Some(self.cmp(other))
+        }
+    }
+
+    let (mut edges, original_total_weight) = {
         let mut text = String::new();
 
         File::open("resources/p107.txt").expect("IO Error?")
             .read_to_string(&mut text).expect("IO Error?");
 
-        let mut total = 0;
         let mut rows = Vec::with_capacity(40);
 
         for line in text.lines() {
@@ -73,7 +89,7 @@ pub fn p107() -> String {
 
             for token in line.split(",") {
                 let maybe_weight = match token.parse::<u64>() {
-                    Ok(w) => { total += w; Some(w) },
+                    Ok(w) => Some(w),
                     Err(_) => None,
                 };
                 row.push(maybe_weight);
@@ -82,17 +98,51 @@ pub fn p107() -> String {
             rows.push(row);
         }
 
-        (rows, total / 2)
+        let mut total = 0;
+        let mut edges = BinaryHeap::new();
+        for i in 0 .. 40 {
+            for j in 0 .. i {
+                if let Some(weight) = rows[i][j] {
+                    edges.push(Edge { start: i, end: j, weight });
+                    total += weight;
+                }
+            }
+        }
+
+        (edges, total)
     };
 
-    fn is_connected<T>(edges: &Vec<Vec<Option<T>>>) -> bool {
-        // PRE: edges is square
+    // then we repeatedly add the smallest edge available that improves connectivity
+    // note it can be "final" because of interior mutability (ugh)
+    let mut components = (0 .. 40)
+        .map(|i| { let mut set = HashSet::new(); set.insert(i); (i, set) })
+        .collect::<HashMap<usize, HashSet<usize>>>();
 
+    let mut first_member = (0 .. 40).map(|i| (i, i)).collect::<HashMap<usize, usize>>();
+
+    let mut total_weight = 0;
+
+    // edges is a minheap so this is greedy -- always add the smallest edge that improves connectivity
+    while !edges.is_empty() {
+        let edge = edges.pop().unwrap();
+        let i = first_member[&edge.start];
+        let j = first_member[&edge.end];
+
+        if components[&i].contains(&j) {
+            continue;
+        }
+
+        // otherwise it's an improvement!
+        let i_comp = components.remove(&i).unwrap();
+        for &k in i_comp.iter() {
+            first_member.insert(k, j);
+            components.get_mut(&j).unwrap().insert(k);
+        }
+
+        total_weight += edge.weight;
     }
 
-
-
-    panic!()
+    (original_total_weight - total_weight).to_string()
 }
 
 pub fn p108() -> String {
