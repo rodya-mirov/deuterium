a0cb4423aaca0dfbb678162b12a71dc40a757901
RIchard Rast <richard.rast@gmail.com>
2018-04-22 11:14:50 -0400

    Solve 164

--- src/main.rs
+++ src/main.rs
@@ -93,6 +93,8 @@ fn do_problem(problem_num: i32) {
 
 
 
+        164 => problems::p151_200::p164(),
+
         169 => problems::p151_200::p169(),
         
         173 => problems::p151_200::p173(),
