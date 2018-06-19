d53792918b30d1430f9409899e1f4473fcfc1649
RIchard Rast <richard.rast@gmail.com>
2018-06-18 21:17:58 -0400

    Comments

--- src/problems/p201_250.rs
+++ src/problems/p201_250.rs
@@ -12,12 +12,16 @@ pub fn p206() -> String {
         }
         true
     }
+    
+    // x^2 ends in 0 so x ends in 0 so x = 10y for some y
+    // y^2 ends in 9 so y ends in 3 or 7, so y=10z+3 or 10z+7 for some z
+    // x^2 has 19 digits so x has 10 digits so y has 9 digits so z has 8 digits
+    // and since x^2 starts with 1, so does y^2, so y^2 < 2 * 10^16 so y < 2 * 10^8
+    // so z < 2 * 10^7 and that makes <10,000,000 choices for z
+    // which means the rest is computationally trivial and further analysis is pointless
 
-    let y_min = pow(10, 8);
-    let y_max = y_min * 2; // actually we'll hit the winner before then
-
-    let z_min = y_min / 10;
-    let z_max = y_max / 10;
+    let z_min = pow(10, 7);
+    let z_max = 2 * pow(10, 7);
 
     for z in z_min .. z_max {
         let y = z*10 + 3;
